5b7de1856bba6829d79f9c11899b989be8bce6a0
robin miller & Tara Nelson <student@epicodus.com>
2014-03-27 15:23:14 -0700

    pretty formatting, very little functionality

--- calendar_ui.rb
+++ calendar_ui.rb
@@ -1,6 +1,8 @@
 require 'active_record'
 require './lib/event'
 require 'date'
+Time.zone = 'America/Los_Angeles'
+require 'validates_timeliness'
 
 ActiveRecord::Base.establish_connection(YAML::load(File.open('./db/config.yml'))['development'])
 
@@ -11,12 +13,21 @@ end
 
 def menu
   puts "Type 'add' to add a new event."
+  puts "Type 'edit' to edit an existing event"
+  puts "Type 'list' to see all of your upcoming events."
+  puts "Type 'options' to see more options."
   puts "Type 'exit' to exit the Calendar Machine."
 
   menu_choice = gets.chomp
   case menu_choice
   when 'add'
     add_event
+  when 'edit'
+    edit_event
+  when 'list'
+    list_events(true)
+  when 'options'
+    options_menu
   when 'exit'
     exit
   else
@@ -42,21 +53,120 @@ def add_event
   new_event = Event.new({:description => description, :location => location, :start_datetime => start_datetime, :end_datetime => end_datetime})
 
   if new_event.save
-    puts "'#{new_event.description}' has been successfully added to the Calendar Machine."
+    puts "'#{new_event.description}' at #{new_event.start_datetime} has been successfully added to the Calendar Machine."
   else
     puts "WARNING! This event was NOT successfully created. The following errors occurred: "
     new_event.errors.full_messages.each { |message| puts message }
     add_event
   end
+end
 
+def edit_event
+  list_events(false)
+  puts "Enter the ID of the event you would like to edit"
+  print ">"
+  event_id = gets.chomp.to_i
+  event_to_edit = Event.find(event_id)
+  continue = nil
+  until continue == 'n'
+    puts "\n\n-Event To Edit-\n"
+    puts "Description: #{event_to_edit.description}"
+    puts "Location: #{event_to_edit.location}"
+    puts "Start: #{event_to_edit.start_datetime.strftime("%m/%d/%y at %I:%M%p")}"
+    puts "End: #{event_to_edit.end_datetime.strftime("%m/%d/%y at %I:%M%p")}"
+    puts "Enter the field you would like to edit."
+    print ">"
+    edit_choice = gets.chomp.downcase
+    attributes = {'description' => :description, 'location' => :location, 'start' => :start_datetime, 'end' => :end_datetime}
+    formats = {'description' => '', 'location' => '', 'start' => ' (MM/DD/YYYY HH:MM)', 'end' => ' (MM/DD/YYYY HH:MM)'}
+    if !attributes[edit_choice].nil?
+      if edit_choice == 'start'
+        new_start_date = get_start_date
+        new_start_time = get_start_time
+        updated_field = "#{new_start_date} #{new_start_time}"
+      elsif edit_choice == 'end'
+        new_end_date = get_end_date('#{event_to_edit.start_datetime.strfdate("%d/%m/%y")}')
+        new_end_time = get_end_time
+        updated_field = "#{new_end_date} #{new_end_time}"
+      else
+        puts "Enter the new value for #{edit_choice}#{formats[edit_choice]}: "
+        updated_field = gets.chomp
+      end
+      if !event_to_edit.update(attributes[edit_choice] => updated_field)
+        puts "WARNING! This event was NOT successfully updated. The following errors occurred: "
+        event_to_edit.errors.full_messages.each { |message| puts message }
+      end
+    else
+      puts "Not a valid choice."
+    end
+    puts "Do you want to edit another field? (Y/N)"
+    continue = gets.chomp.downcase
+  end
+end
+
+def list_events(flag)
+  puts "*************Your Events***********\n"
+  events_in_order = Event.order(:start_datetime, :end_datetime)
+  puts "\n\n"
+  events_in_order.each do |event|
+    if (event.start_datetime >= DateTime.now) && flag
+      length = event.description.length
+      padding = 25 - length
+      if event.id < 10
+        event_id_format = "#{0}#{event.id}"
+      else
+        event_id_format = "#{event.id}"
+      end
+      puts "ID #{event_id_format}: #{event.description}: #{" "*padding} #{event.start_datetime.strftime("%m/%d/%y at %I:%M%p")} to #{event.end_datetime.strftime("%m/%d/%y at %I:%M%p")}.\n"
+    elsif !flag
+      puts "ID #{event_id_format}: #{event.description}: #{" "*padding} #{event.start_datetime.strftime("%m/%d/%y at %I:%M%p")} to #{event.end_datetime.strftime("%m/%d/%y at %I:%M%p")}.\n"
+    end
+  end
+  puts "\n\n"
+end
+
+def options_menu
+  puts "===========Options Menu==========="
+  puts "\n\nType 'month' to see events for the current month."
+  puts "Type 'week' to see events for the current week."
+  puts "Type 'today' to see today's events."
+  puts "Type 'main' to return to the main menu."
+  print ">"
+  option_choice = gets.chomp
+  case option_choice
+  when 'month'
+    view_by_month
+  when 'week'
+    view_by_week
+  when 'today'
+    view_by_day
+  when 'main'
+    main
+  else
+    puts "That was not a valid choice."
+    options_menu
+  end
+end
+
+def view_by_month
+  today = DateTime.now
+  puts "\n\n*******#{today.strftime('%B')} Events********\n\n"
+  month_events = Event.by_month(today.month)
+  puts month_events.first.description
+end
+
+def view_by_week
+end
+
+def view_by_day
 end
 
 def get_start_date
-  puts "Enter the start date (DD/MM/YYYY): "
+  puts "Enter the start date (MM/DD/YYYY): "
   print ">"
   start_date = gets.chomp
   begin
-    start_date = DateTime.parse(start_date)
+    start_date = Date.strptime(start_date, '%m/%d/%Y')
   rescue
     puts "Please enter a valid date."
     get_start_date
@@ -70,7 +180,7 @@ def get_start_time
   print ">"
   start_time = gets.chomp
   begin
-    start_time = Time.parse(start_time)
+    Time.parse(start_time)
   rescue
     puts "Please enter a valid time."
     get_start_time
@@ -83,17 +193,20 @@ def get_end_date(start_date)
   puts "Is this a multi-day event? (Y/N)"
   multi_day = gets.chomp.downcase
   if multi_day == 'y'
-    puts "Enter the end date (DD/MM/YYYY): "
+    puts "Enter the end date (MM/DD/YYYY): "
     print ">"
     end_date = gets.chomp
     begin
-      end_date = DateTime.parse(end_date)
+      end_date = Date.strptime(end_date, '%m/%d/%Y')
     rescue
       puts "Please enter a valid date."
       get_end_date
     end
-  else
+  elsif multi_day == 'n'
     end_date = start_date
+  else
+    puts "Not a valid input."
+    get_end_date(start_date)
   end
   end_date
 end
@@ -103,7 +216,7 @@ def get_end_time
   print ">"
   end_time = gets.chomp
   begin
-    end_time = Time.parse(end_time)
+    Time.parse(end_time)
   rescue
     puts "Please enter a valid time."
     get_end_time
--- lib/event.rb
+++ lib/event.rb
@@ -1,19 +1,19 @@
-require 'pry'
-
+require 'validates_timeliness'
 class Event < ActiveRecord::Base
   I18n.enforce_available_locales = false
   validates :description, :presence => true, :length => { :maximum => 30 }
   validates :location, :presence => true, :length => { :maximum => 25 }
   validates :start_datetime, :presence => true
   validates :end_datetime, :presence => true
-  validate :start_datetime_before_end_datetime
+  # validate :start_datetime_before_end_datetime
+  validates_datetime :end_datetime, :after => :start_datetime
 
 private
-  def start_datetime_before_end_datetime
-    unless start_datetime.nil? || end_datetime.nil?
-      if start_datetime > end_datetime
-        errors.add(:end_datetime, "An event can't end before it starts")
-      end
-    end
-  end
+  # def start_datetime_before_end_datetime
+  #   unless start_datetime.nil? || end_datetime.nil?
+  #     if start_datetime > end_datetime
+  #       errors.add(:start_datetime, ": An event can't end before it starts")
+  #     end
+  #   end
+  # end
 end
